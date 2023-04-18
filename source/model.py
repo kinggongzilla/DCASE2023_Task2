834f5240ddf107aa24d195c393f5af368a82ec4c
David Hauser <hauser-david@hotmail.com>
2023-04-18 23:19:09 +0200

    fix model class

--- source/model.py
+++ source/model.py
@@ -10,14 +10,14 @@ class CNNAutoencoder(nn.Module):
         # Encoder
         self.conv1 = nn.Conv2d(1, 16, kernel_size=3)
         self.bn1 = nn.BatchNorm2d(16)
-        self.conv1 = nn.Conv2d(16, 16, kernel_size=3)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 8, kernel_size=3)
-        self.bn2 = nn.BatchNorm2d(8)
-        self.conv3 = nn.Conv2d(8, 4, kernel_size=3)
-        self.bn3 = nn.BatchNorm2d(4)
-        self.conv4 = nn.Conv2d(4, 4, kernel_size=3)
+        self.conv2 = nn.Conv2d(16, 16, kernel_size=3)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 8, kernel_size=3)
+        self.bn3 = nn.BatchNorm2d(8)
+        self.conv4 = nn.Conv2d(8, 4, kernel_size=3)
         self.bn4 = nn.BatchNorm2d(4)
+        self.conv5 = nn.Conv2d(4, 4, kernel_size=3)
+        self.bn5 = nn.BatchNorm2d(4)
         
         #compressed latent space size: ???
 
@@ -29,8 +29,8 @@ class CNNAutoencoder(nn.Module):
         self.t_conv3 = nn.ConvTranspose2d(8, 16, kernel_size=3)
         self.t_bn3 = nn.BatchNorm2d(16)
         self.t_conv4 = nn.ConvTranspose2d(16, 16, kernel_size=3)
-        self.t_bn3 = nn.BatchNorm2d(16)
-        self.t_conv4 = nn.ConvTranspose2d(16, 1, kernel_size=3)
+        self.t_bn4 = nn.BatchNorm2d(16)
+        self.t_conv5 = nn.ConvTranspose2d(16, 1, kernel_size=3)
 
         #output size: (1, 80, 63), 1 channel, 80x63 pixels
 
@@ -41,11 +41,13 @@ class CNNAutoencoder(nn.Module):
         x = torch.relu(self.bn2(self.conv2(x)))
         x = torch.relu(self.bn3(self.conv3(x)))
         x = torch.relu(self.bn4(self.conv4(x)))
+        x = torch.relu(self.bn5(self.conv5(x)))
         
         # Decoder
         x = torch.relu(self.t_bn1(self.t_conv1(x)))
         x = torch.relu(self.t_bn2(self.t_conv2(x)))
         x = torch.relu(self.t_bn3(self.t_conv3(x)))
-        x = torch.sigmoid(self.t_conv4(x))
+        x = torch.relu(self.t_bn4(self.t_conv4(x)))
+        x = torch.sigmoid(self.t_conv5(x))
         
         return x
