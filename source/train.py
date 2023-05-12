76d6cc3e65b3b1d525dba2bdda854f521196bb36
tobiaskatch <tobis.katsch42@gmail.com>
2023-05-12 23:33:16 +0200

    some changes to ensure file namings are aligned in case that this is the issue

--- source/train.py
+++ source/train.py
@@ -44,9 +44,9 @@ def train(model, optimizer, train_loader, test_loader, machine_name):
             wandb.log({f"{machine_name}_step_loss": batch_loss}, step=step_count)
 
             if step_count % LOG_EVERY == 0:
-                model.load_state_dict(torch.load(save_path))
-                model.eval()
-                model.to(device)
+                #model.load_state_dict(torch.load(save_path))
+                #model.eval()
+                #model.to(device)
                 test(model, test_loader, machine_name)
                 model.train()
 
