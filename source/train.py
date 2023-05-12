43d424d4c6cbecf3af41702fcea5a4d15a6b877c
tobiaskatch <tobis.katsch42@gmail.com>
2023-05-13 01:24:02 +0200

    log train anomaly/normal losses (not only test like before)

--- source/train.py
+++ source/train.py
@@ -38,20 +38,20 @@ def train(model, optimizer, train_loader, test_loader, machine_name):
             outputs = model.forward(inputs)
 
             # compute loss without reduction
-            unreduced_loss = loss_func(outputs, targets, reduction='none')
-            unreduced_loss = unreduced_loss[masks == 0].view(-1)
-
-            # Mask labels to align with unreduced_loss
-            masked_labels = labels[masks == 0].view(-1)
+            unmasked_loss = loss_func(outputs, targets)
 
             if step_count % LOG_EVERY == 0:
+
                 # Split unreduced_loss by labels (labels == IS_ANOMAL or labels == IS_NORMAL)
-                normal_indices = (masked_labels == IS_NORMAL)
-                anomal_indices = (masked_labels == IS_ANOMALY)
+                normal_indices = (labels == IS_NORMAL)
+                anomal_indices = (labels == IS_ANOMALY)
+
+                normal_masks = masks[normal_indices]
+                anomal_masks = masks[anomal_indices]
 
                 # Compute and log both average losses
-                train_loss_normal = torch.mean(unreduced_loss[normal_indices])
-                train_loss_anomaly = torch.mean(unreduced_loss[anomal_indices])
+                train_loss_normal = torch.mean(unmasked_loss[normal_indices][normal_masks == 0])
+                train_loss_anomaly = torch.mean(unmasked_loss[anomal_indices][anomal_masks == 0])
 
                 # Log loss separately for normal and anomaly with the step number
                 wandb.log({f"{machine_name}_train_loss_anomaly": train_loss_anomaly}, step=step_count)
@@ -62,7 +62,7 @@ def train(model, optimizer, train_loader, test_loader, machine_name):
                 num_anomaly = anomal_indices.sum().float()
                 train_loss = (num_normal * train_loss_normal + num_anomaly * train_loss_anomaly) / (num_normal + num_anomaly)
             else:
-                train_loss = torch.mean(unreduced_loss)
+                train_loss = torch.mean(unmasked_loss[masks == 0].view(-1))
 
             train_loss.backward()
             optimizer.step()
